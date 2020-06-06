ecbdd2fa1ac6cadde13b5ec640221137c2a78bca
Marian Akiwumi <mfakiwumi1992@yahoo.com>
2020-06-06 18:57:14 -0400

    deleted files and created main, search and table to display

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
 import React from 'react';
-import Wrapper from './components/wrapper';
-import Main from './components/main';
-import Header from './components/header';
+import Main from './components/Main';
+
 
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Wrapper>
-        <Header />
-        <Main />
-      </Wrapper>
+      <Main />
     </div>
   );
 }
---
+++ src/components/SearchBar.js
@@ -0,0 +1,52 @@
+import React from "react";
+import "../style/search.css";
+
+function SearchBar(props) {
+
+    return (
+        <div className="container-fluid">
+
+
+            <header className="z-depth-3 col s12" >
+                <div className="row">
+
+                    <div className="col s6 ">
+
+                        <div className="col s6 inputAndButton right-align ">
+
+                            <input
+                                onChange={props.handleInputChange}
+                                value={props.value}
+                                id="employees"
+                                type="text"
+                                name="search"
+                                list="employee"
+                                className=" inputBox  "
+                                placeholder="Search by name" />
+
+
+                        </div>
+                        <div className="col m4">
+
+
+                            <button
+                                type="submit"
+                                value=""
+                                className=" btn z-depth-2 waves-effect  searchBttn"
+                                onClick={props.handleSearch} >Search</button>
+                        </div>
+
+
+
+
+
+
+                    </div>
+                    
+                </div>
+            </header>
+        </div>
+    )
+}
+
+export default SearchBar
