e68f516728f262bdc076fb716b2f65b634912748
Marian Akiwumi <mfakiwumi1992@yahoo.com>
2020-06-07 21:52:14 -0400

    Updates

---
+++ precache-manifest.2178b5bc0fd466c78c8066ca4e75ce67.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbc7596421ef8001807c6ebd9254d61c",
+    "url": "/Employee-Directory/index.html"
+  },
+  {
+    "revision": "26952bf48ba6d5fa8c9a",
+    "url": "/Employee-Directory/static/css/main.f52b4914.chunk.css"
+  },
+  {
+    "revision": "372e3918265f9071b8f8",
+    "url": "/Employee-Directory/static/js/2.1f4bcf48.chunk.js"
+  },
+  {
+    "revision": "0a572ad323474bafc6a871b6336141bd",
+    "url": "/Employee-Directory/static/js/2.1f4bcf48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26952bf48ba6d5fa8c9a",
+    "url": "/Employee-Directory/static/js/main.8b2c5b23.chunk.js"
+  },
+  {
+    "revision": "9d1e2e8462d05b5a5ab8",
+    "url": "/Employee-Directory/static/js/runtime-main.b1a1747f.js"
+  }
+]);
